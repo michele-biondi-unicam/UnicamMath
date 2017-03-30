033c6f0b8b68c28c003c228395372645e0a6c012
Michele Biondi <michele.biondi@studenti.unicam.it>
2017-03-30 23:25:43 +0200

    Implementati input dei 2 cateti. Li richiede fino a che non è inserito un valore valido e tengono conto della 'cultura' del thread corrente.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace UnicamMath
 {
@@ -6,18 +7,42 @@ namespace UnicamMath
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Questo programma calcola l'ipotenusa e gli angoli interni fornendo la lunghezza di due cateti");
-
-            Console.Write("Inserisci la lunghezza del primo cateto: ");
-            string cateto1Testuale = Console.ReadLine();
-
-            Console.Write("Inserisci la lunghezza del secondo cateto: ");
-            string cateto2Testuale = Console.ReadLine();
-
+            double cateto1;
+            double cateto2;
             double ipotenusa = 0;
             double angolo1 = 90;
             double angolo2 = 0;
             double angolo3 = 0;
+
+            Console.WriteLine("Questo programma calcola l'ipotenusa e gli angoli interni fornendo la lunghezza di due cateti");
+
+            // Chiedo all'utente la lunghezza del primo cateto finchè non viene inserito un valore accettabile.
+            while(true){
+                Console.Write("Inserisci la lunghezza del primo cateto: ");
+                string cateto1Testuale = Console.ReadLine();
+                try{
+                    cateto1 = double.Parse(cateto1Testuale, CultureInfo.CurrentCulture); // Interpreta il metodo di rappresentazione dei numeri in base al thread corrente.
+                } catch(Exception ex) {
+                    Console.WriteLine("Non ho capito. Ritenta.");
+                    continue;
+                }
+                break;
+            }
+
+            // Chiedo all'utente la lunghezza del secondo cateto finchè non viene inserito un valore accettabile.
+            while(true){
+                Console.Write("Inserisci la lunghezza del secondo cateto: ");
+                string cateto2Testuale = Console.ReadLine();
+                try{
+                    cateto2 = double.Parse(cateto2Testuale, CultureInfo.CurrentCulture); // Interpreta il metodo di rappresentazione dei numeri in base al thread corrente.
+                } catch(Exception ex) {
+                    Console.WriteLine("Non ho capito. Ritenta.");
+                    continue;
+                }
+                break;
+            }
+
+            
 
             //TO DO
 
